9ebf681df500e362616d65cfc62ca9d3117d8fc3
Ritik Jain <ritikjain666@gmail.com>
2022-08-19 14:22:09 +0530

    gh page router

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
